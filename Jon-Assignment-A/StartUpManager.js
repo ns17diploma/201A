143f4c5a171265af0479bb05e1d5d7352b74d664
NS <nonstopacademy@gmail.com>
2018-02-08 12:07:44 +0800

    replace

---
+++ Jon-Assignment-A/StartUpManager.js
@@ -0,0 +1,41 @@
+const $ = require('jquery');
+const jsf = require('jsonfile');
+const fs = require('fs');
+const filename = ('jsfile.json');
+const Validator = require('./validate.js')
+
+let answer = new Validator();
+
+/*
+* Createed Class StartUp
+*/
+class Start_Up{
+
+	/* Call this function */
+	constructor(){
+		this.Ready();
+		// this.Read_File();
+		// this.Record();
+	}
+
+	/* Function */	
+	Ready(){
+		if (!fs.existsSync(filename)) {
+			jsf.writeFileSync(filename,[]);
+		}
+	}
+
+	Read_File(){
+		return jsf.readFileSync(filename);
+	}
+
+	Record(){
+		console.log(answer)
+		var json = this.Read_File('jsfile.json');
+		json.push();
+		jsf.writeFileSync('jsfile.json',json);  
+	}
+};
+module.exports = Start_Up
+
+
